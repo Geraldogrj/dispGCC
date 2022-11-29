99f409e64a4e3d62004185c16b0808d041a6af06
geraldogrj <geraldogrj@gmail.com>
2022-11-29 14:34:19 -0300

    cadastro de equipamentos

--- equipamentos/urls.py
+++ equipamentos/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('home/<int:id>', views.home, name='home'),
     path('cadastrar/categoria', views.cadastrar_categoria, name='cadastrar_categoria'),
     path('ver_equipamento/<int:id>', views.ver_equipamento, name = "ver_equipamento"),
-    path('', views.ver_esquadroes, name = "ver_esquadroes")
+    path('', views.ver_esquadroes, name = "ver_esquadroes"),
+    path('cadastrar/equipamento', views.cadastrar_equipamento, name = 'cadastrar_equipamento'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- equipamentos/views.py
+++ equipamentos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
-from .forms import FormCategoria
+from .forms import FormCategoria, FormEquipamento
 from django.contrib import messages
 from .models import Categoria, Esquadrao, Equipamento
 
@@ -7,6 +7,7 @@ def home (request, id):
     esquadrao = Esquadrao.objects.get(id = id)
     form_categoria = FormCategoria()
     categorias = Categoria.objects.all()
+    form_equipamento = FormEquipamento()
     
     equipamentos = Equipamento.objects.filter(esquadrao = esquadrao)
     set_categorias = set(categorias)
@@ -15,7 +16,8 @@ def home (request, id):
         'form_categoria' : form_categoria,
         'categorias' : set_categorias,
         'esquadrao' : esquadrao,
-        'equipamentos' : equipamentos
+        'equipamentos' : equipamentos,
+        'form_equipamento' : form_equipamento,
     }
     return render (request, 'home.html', context)
 
@@ -33,15 +35,35 @@ def cadastrar_categoria(request):
 
 def ver_equipamento(request, id):
     equipamento = Equipamento.objects.get(id = id)
+    form_categoria = FormCategoria()
+    form_equipamento = FormEquipamento()
     context = {
-        'equipamento' : equipamento
+        'equipamento' : equipamento,
+        'form_categoria': form_categoria,
+        'form_equipamento' : form_equipamento,
     }
     return render (request, 'ver_equipamento.html', context)
 
 def ver_esquadroes(request):
-    esquadroes = Esquadrao.objects.all();
+    esquadroes = Esquadrao.objects.all()
+    form_categoria = FormCategoria()
+    form_equipamento = FormEquipamento()
     
     context = {
         'esquadroes' : esquadroes,
+        'form_categoria':form_categoria,
+        'form_equipamento' : form_equipamento,
     }
     return render (request, 'ver_esquadroes.html', context)
+
+def cadastrar_equipamento(request):
+    if request.method == 'POST':
+        form_equipamento = FormEquipamento(request.POST)
+        
+        if form_equipamento.is_valid():
+            messages.success(request, 'Equipamento Cadastrado com sucesso')
+            form_equipamento.save()
+            return redirect ('/')
+        else:
+            messages.error(request, 'O equipamento não pode ser cadastrado')
+            return redirect ('/')
